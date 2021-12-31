3322a7159301d30f6408b5b11286b76ea941a6e8
saves7 <a_v.efimov@mail.ru>
2021-12-31 17:31:46 +0500

    js udemy part 3

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ let numberOfFilms;
 function start () {
     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели','');
     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели','');
     }
 }
 
@@ -16,17 +18,38 @@ let personalMovieDb = {
     privat: false,
 };
 
-for (let i=0; i<2; i++) {
-    let a = prompt('Фильм'); 
-    let b = prompt('Рейтинг'); 
-    if (a != null && b != null && a !='' && b !='' ) {
-        personalMovieDb.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
+function rememberMyFilms () {
+
+    for (let i=0; i<2; i++) {
+        let a = prompt('Фильм'); 
+        let b = prompt('Рейтинг'); 
+        if (a != null && b != null && a !='' && b !='' ) {
+            personalMovieDb.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+     
     }
-    
 }
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    for (let i=0; i<2; i++) {
+        let a = prompt('Фильм'); 
+        let b = prompt('Рейтинг'); 
+        if (a != null && b != null && a !='' && b !='' ) {
+            personalMovieDb.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
+
+detectPersonalLevel();
 
 console.log(personalMovieDb);
